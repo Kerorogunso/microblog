1eaade419a6a84f4323b8270243ece543652eb45
Albert <albertswchung@gmail.com>
2018-06-07 15:45:01 +0100

    test

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['you-email@example.com']
+    ADMINS = ['test']
     POSTS_PER_PAGE = 3
